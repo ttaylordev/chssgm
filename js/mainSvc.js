01abbe5f429b6cf9ecbe049abaf663c41b281dd6
Tyson Taylor <mellowt@gmail.com>
2016-01-09 10:48:58 -0700

    getting closer to connectivity

--- js/mainSvc.js
+++ js/mainSvc.js
@@ -1,6 +1,8 @@
-angular.module('app').service('mainSvc', function() {
+angular.module('app')
+.service('mainSvc', function() {
   this.getMainSvc = function() {
     var mainSvc = 'mainSvc is also working';
+    console.log('mainSvc is logging');
     return mainSvc;
   };
 });
--- js/view/loginCtrl.js
+++ js/view/loginCtrl.js
@@ -1,5 +1,7 @@
 angular.module('app').controller('loginCtrl', function($scope, loginSvc) {
-  $scope.testLoginCtrl = loginSvc.getTested();
-  // 'loginCtrl is working in separate file';
+  $scope.newFunc = function() {};
+  $scope.testLoginCtrl = 'loginCtrl is working';
+  $scope.loginSvc = loginSvc.getLoginSvc();
   console.log('loginCtrl is logging');
+
 });
